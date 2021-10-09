382e38c1b8e93bb40e90e8067383fd88a7b4b3e5
Alex <alexandepp@gmail.com>
2021-10-09 18:30:51 +0300

    my verstka

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import style from './Skills.module.css'
 import styleContainer from "./../common/styles/Container.module.css";
+import Skill from "./skill/Skill";
 
 function Skills() {
     return (
         <div className={style.skillsBlock}>
-            <div className={`${style.skillsContainer} ${styleContainer.container}`}>
+            <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>Skills</h2>
-                <div className={style.skills}></div>
+                <div className={style.skills}>
+                <Skill title={'JS'} description={'TypeScript builds on top of JavaScript. First, you write the TypeScript code. Then, you compile the TypeScript code into plain JavaScript code using a TypeScript compiler.'}/>
+                <Skill title={'CSS'} description={'Cascading Style Sheets,  is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.'}/>
+                <Skill title={'REACT'} description={'React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”.'}/>
+                </div>
+
             </div>
         </div>
     );
